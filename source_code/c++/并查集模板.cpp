bd8552fdf94e4d30e96f4d1f4aa4b838356a06af
zero-range <111559760+zero-range@users.noreply.github.com>
2022-09-23 21:21:23 +0800

    Update 并查集模板.cpp

--- source_code/c++/并查集模板.cpp
+++ source_code/c++/并查集模板.cpp
@@ -1,10 +1,9 @@
 #define maxl 1000
-int fa[maxl], rank[maxl];
-ll X[maxl], Y[maxl], Z[maxl];
+int fa[maxl], v[maxl];
 inline void init(int n){
     for(int i=1;i<=n;++i){
         fa[i]=i;
-        rank[i]=1;
+        v[i]=1;
     }
 }
 int find(int x){
@@ -12,7 +11,7 @@ int find(int x){
 }
 inline void merge(int i, int j){
     int x=find(i),y=find(j);
-    if (rank[x]<=rank[y]) fa[x]=y;
+    if (v[x]<=v[y]) fa[x]=y;
     else fa[y]=x;
-    if (rank[x]==rank[y]&&x!=y) rank[y]++;
+    if (v[x]==v[y]&&x!=y) v[y]++;
 }
